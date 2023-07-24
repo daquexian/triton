9cedfe7b2c4e9144808c6fd892ede976ce3ff76f
daquexian <daquexian566@gmail.com>
2023-07-24 17:39:53 +0800

    new rwkv mha fwd
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- python/tutorials/09-rwkv-mha.py
+++ python/tutorials/09-rwkv-mha.py
@@ -25,7 +25,7 @@ def max_fn(x, y):
 
 @triton.jit
 def _fwd_kernel(
-    Q, K, V,
+    Q, K, V, Decay, First,
     Out,
     stride_qz, stride_qh, stride_qm, stride_qk,
     stride_kz, stride_kh, stride_kn, stride_kk,
@@ -72,7 +72,8 @@ def _fwd_kernel(
     # loop over k, v and update accumulator
     lo = 0
     hi = (start_m + 1) * BLOCK_M
-    decay = 1 - tl.math.pow(2., -5. - off_hz % H)
+    decay = tl.load(Decay + off_hz % H)
+    first = tl.load(First + off_hz % H)
     for start_n in range(lo, hi, BLOCK_N):
         # -- load k, v --
         k = tl.load(K_block_ptr)
@@ -80,7 +81,8 @@ def _fwd_kernel(
         # -- compute qk ---
         qk = tl.dot(q, k)
         powers = (offs_m[:, None] - (offs_n + start_n)[None, :])
-        att_mask = tl.where(powers >= 0, tl.math.fast_powf(decay, powers), 0)
+        att_mask = tl.where(powers > 0, tl.math.fast_powf(decay, powers - 1), 0).to(tl.float16)
+        att_mask += tl.where(powers == 0, first, 0)
         qk *= att_mask
         acc += tl.dot(qk.to(tl.float16), v)
         # update pointers
@@ -99,13 +101,96 @@ def _fwd_kernel(
     tl.store(O_block_ptr, acc.to(tl.float16))
 
 
+# @triton.jit
+def _bwd_kernel(
+    Q, K, V, sm_scale, Out, DO,
+    DQ, DK, DV,
+    L,
+    D,
+    stride_qz, stride_qh, stride_qm, stride_qk,
+    stride_kz, stride_kh, stride_kn, stride_kk,
+    stride_vz, stride_vh, stride_vk, stride_vn,
+    Z, H, N_CTX,
+    num_block,
+    BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    CAUSAL: tl.constexpr,
+):
+    off_hz = tl.program_id(0)
+    off_z = off_hz // H
+    off_h = off_hz % H
+    qk_scale = sm_scale * 1.44269504
+    # offset pointers for batch/head
+    Q += off_z * stride_qz + off_h * stride_qh
+    K += off_z * stride_qz + off_h * stride_qh
+    V += off_z * stride_qz + off_h * stride_qh
+    DO += off_z * stride_qz + off_h * stride_qh
+    DQ += off_z * stride_qz + off_h * stride_qh
+    DK += off_z * stride_qz + off_h * stride_qh
+    DV += off_z * stride_qz + off_h * stride_qh
+    for start_n in range(0, num_block):
+        lo = start_n * BLOCK_M
+        # initialize row/col offsets
+        offs_qm = lo + tl.arange(0, BLOCK_M)
+        offs_n = start_n * BLOCK_M + tl.arange(0, BLOCK_M)
+        offs_m = tl.arange(0, BLOCK_N)
+        offs_k = tl.arange(0, BLOCK_DMODEL)
+        # initialize pointers to value-like data
+        q_ptrs = Q + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        k_ptrs = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        v_ptrs = V + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        do_ptrs = DO + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        dq_ptrs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        # pointer to row-wise quantities in value-like data
+        D_ptrs = D + off_hz * N_CTX
+        l_ptrs = L + off_hz * N_CTX
+        # initialize dv amd dk
+        dv = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+        dk = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+        # k and v stay in SRAM throughout
+        k = tl.load(k_ptrs)
+        v = tl.load(v_ptrs)
+        # loop over rows
+        for start_m in range(lo, num_block * BLOCK_M, BLOCK_M):
+            offs_m_curr = start_m + offs_m
+            # load q, k, v, do on-chip
+            q = tl.load(q_ptrs)
+            # recompute p = softmax(qk, dim=-1).T
+            qk = tl.dot(q, tl.trans(k))
+            qkm = qk
+            # compute dv
+            do = tl.load(do_ptrs)
+            dv += tl.dot(tl.trans(qkm.to(Q.dtype.element_ty)), do)
+            # compute dp = dot(v, do)
+            Di = tl.load(D_ptrs + offs_m_curr)
+            dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - Di[:, None]
+            dp += tl.dot(do, tl.trans(v))
+            # compute ds = p * (dp - delta[:, None])
+            ds = p * dp * sm_scale
+            # compute dk = dot(ds.T, q)
+            dk += tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
+            # compute dq
+            dq = tl.load(dq_ptrs)
+            dq += tl.dot(ds.to(Q.dtype.element_ty), k)
+            tl.store(dq_ptrs, dq)
+            # increment pointers
+            dq_ptrs += BLOCK_M * stride_qm
+            q_ptrs += BLOCK_M * stride_qm
+            do_ptrs += BLOCK_M * stride_qm
+        # write-back
+        dv_ptrs = DV + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        dk_ptrs = DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        tl.store(dv_ptrs, dv)
+        tl.store(dk_ptrs, dk)
+
+
 empty = torch.empty(128, device="cuda")
 
 
 class _attention(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, q, k, v):
+    def forward(ctx, q, k, v, decay, first):
         # shape constraints
         Lq, Lk, Lv = q.shape[-1], k.shape[-1], v.shape[-1]
         assert Lq == Lk and Lk == Lv
@@ -117,7 +202,7 @@ class _attention(torch.autograd.Function):
 
         num_warps = 4 if Lk <= 64 else 8
         _fwd_kernel[grid](
-            q, k, v,
+            q, k, v, decay, first,
             o,
             q.stride(0), q.stride(1), q.stride(2), q.stride(3),
             k.stride(0), k.stride(1), k.stride(2), k.stride(3),
@@ -143,9 +228,9 @@ attention = _attention.apply
 
 
 def pytorch_mha(r, k, v, att_mask):
-    x = (r @ k.transpose(-2, -1))
-    x *= att_mask
-    ref_out = x @ v
+    qk = (r @ k.transpose(-2, -1))
+    qkm = qk * att_mask
+    ref_out = qkm @ v
     return ref_out
 
 
@@ -157,16 +242,19 @@ def test_op(Z, H, N_CTX, D_HEAD, dtype=torch.float16):
     v = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0., std=0.5).requires_grad_()
     # reference implementation
     att_mask = torch.zeros(H, N_CTX, N_CTX, dtype=dtype, device="cuda")
+    decay = torch.arange(0.9, 0.9 + H * 0.01, 0.01, device='cuda')
+    first = torch.arange(0.95, 0.95 + H * 0.001, 0.001, device='cuda')
+    rows, cols = torch.tril_indices(N_CTX, N_CTX, device='cuda')
+    powers = rows - cols - 1
     for h in range(H):
-        rows, cols = torch.tril_indices(N_CTX, N_CTX, device='cuda')
-        powers = rows - cols
-        decay = 1 - 2 ** (-5 - h)
-        att_mask[h][rows, cols] = (decay ** powers).half()
+        att_mask[h][rows, cols] = (decay[h] ** powers).half()
+        att_mask[h].fill_diagonal_(first[h].item())
     ref_out = pytorch_mha(r, k, v, att_mask)
 
     # triton implementation
-    tri_out = attention(r, k, v)
+    tri_out = attention(r, k, v, decay, first)
 
+    import pdb; pdb.set_trace()
     # compare
     assert torch.allclose(ref_out, tri_out, atol=5e-2, rtol=2e-3)
 
@@ -177,8 +265,8 @@ configs = [triton.testing.Benchmark(
     x_names=['N_CTX'],
     x_vals=[2**i for i in range(7, 12)],
     line_arg='provider',
-    line_vals=['pytorch', 'triton'],
-    line_names=['pytorch', 'Triton'],
+    line_vals=['triton', 'pytorch'],
+    line_names=['triton', 'pytorch'],
     styles=[('red', '-'), ('blue', '-')],
     ylabel='ms',
     plot_name=f'fused-attention-batch{BATCH}-head{N_HEADS}-d{D_HEAD}-{mode}',
@@ -194,20 +282,24 @@ def bench_flash_attention(BATCH, H, N_CTX, D_HEAD, mode, provider, dtype=torch.f
     q = torch.randn((BATCH, H, N_CTX, D_HEAD), dtype=dtype, device="cuda", requires_grad=True)
     k = torch.randn((BATCH, H, N_CTX, D_HEAD), dtype=dtype, device="cuda", requires_grad=True)
     v = torch.randn((BATCH, H, N_CTX, D_HEAD), dtype=dtype, device="cuda", requires_grad=True)
+
+    att_mask = torch.zeros(H, N_CTX, N_CTX, dtype=dtype, device="cuda")
+    decay = torch.arange(0.9, 0.9 + H * 0.01, 0.01, device='cuda')
+    first = torch.arange(0.95, 0.95 + H * 0.001, 0.001, device='cuda')
+    rows, cols = torch.tril_indices(N_CTX, N_CTX, device='cuda')
+    powers = rows - cols - 1
+    for h in range(H):
+        att_mask[h][rows, cols] = (decay[h] ** powers).half()
+        att_mask[h].fill_diagonal_(first[h].item())
+
     if provider == "triton":
-        fn = lambda: attention(q, k, v)
+        fn = lambda: attention(q, k, v, decay, first)
         if mode == 'bwd':
             o = fn()
             do = torch.randn_like(o)
             fn = lambda: o.backward(do, retain_graph=True)
         ms = triton.testing.do_bench(fn, warmup=warmup, rep=rep)
     if provider == "pytorch":
-        att_mask = torch.zeros(H, N_CTX, N_CTX, dtype=dtype, device="cuda")
-        for h in range(H):
-            rows, cols = torch.tril_indices(N_CTX, N_CTX, device='cuda')
-            powers = rows - cols
-            decay = 1 - 2 ** (-5 - h)
-            att_mask[h][rows, cols] = (decay ** powers).half()
         fn = lambda: pytorch_mha(q, k, v, att_mask)
         if mode == 'bwd':
             o = fn()
