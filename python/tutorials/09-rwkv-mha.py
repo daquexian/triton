1d98b380732ac2f931bc47d0d300ac67c1195b93
daquexian <daquexian566@gmail.com>
2023-07-25 11:50:31 +0800

    refine
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- python/tutorials/09-rwkv-mha.py
+++ python/tutorials/09-rwkv-mha.py
@@ -1,14 +1,5 @@
 """
-Fused Attention
-===============
-
-This is a Triton implementation of the Flash Attention v2 algorithm from Tri Dao (https://tridao.me/publications/flash2/flash2.pdf)
-
-Extra Credits:
-- Original flash attention paper (https://arxiv.org/abs/2205.14135)
-- Rabe and Staats (https://arxiv.org/pdf/2112.05682v2.pdf)
-- Adam P. Goucher for simplified vector math
-
+Based on python/tutorials/06-fused-attention.py
 """
 
 import pytest
@@ -16,11 +7,6 @@ import torch
 
 import triton
 import triton.language as tl
-
-
-@triton.jit
-def max_fn(x, y):
-    return tl.math.max(x, y)
 
 
 @triton.jit
@@ -56,108 +42,6 @@ def _fwd_kernel(
     )
     V_block_ptr = tl.make_block_ptr(
         base=V + qvk_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
-        strides=(stride_vk, stride_vn),
-        offsets=(0, 0),
-        block_shape=(BLOCK_N, BLOCK_DMODEL),
-        order=(1, 0)
-    )
-    # initialize offsets
-    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
-    offs_n = tl.arange(0, BLOCK_N)
-    acc = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
-    # load q: it will stay in SRAM throughout
-    q = tl.load(Q_block_ptr)
-    q = q.to(tl.float16)
-    # loop over k, v and update accumulator
-    lo = 0
-    hi = (start_m + 1) * BLOCK_M
-    decay = tl.load(Decay + off_hz % H)
-    first = tl.load(First + off_hz % H)
-    for start_n in range(lo, hi, BLOCK_N):
-        # -- load k, v --
-        k = tl.load(K_block_ptr)
-        v = tl.load(V_block_ptr)
-        # -- compute qk ---
-        qk = tl.dot(q, k)
-        powers = (offs_m[:, None] - (offs_n + start_n)[None, :])
-        att_mask = tl.where(powers > 0, tl.math.fast_powf(decay, powers - 1), 0).to(tl.float16)
-        att_mask += tl.where(powers == 0, first, 0)
-        qk *= att_mask
-        acc += tl.dot(qk.to(tl.float16), v)
-        # update pointers
-        K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_N))
-        V_block_ptr = tl.advance(V_block_ptr, (BLOCK_N, 0))
-    # write back l and m
-    # write back O
-    O_block_ptr = tl.make_block_ptr(
-        base=Out + qvk_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
-        strides=(stride_om, stride_on),
-        offsets=(start_m * BLOCK_M, 0),
-        block_shape=(BLOCK_M, BLOCK_DMODEL),
-        order=(1, 0)
-    )
-    tl.store(O_block_ptr, acc.to(tl.float16))
-
-
-# @triton.jit
-def _bwd2_kernel(
-    Q, K, V, Decay, First, Out, DO,
-    DQ, DK, DV, DDecay, DFirst,
-    stride_qz, stride_qh, stride_qm, stride_qk,
-    stride_kz, stride_kh, stride_kn, stride_kk,
-    stride_vz, stride_vh, stride_vk, stride_vn,
-    stride_oz, stride_oh, stride_om, stride_on,
-    Z, H, N_CTX,
-    BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
-    BLOCK_N: tl.constexpr,
-):
-    start_m = tl.program_id(0)
-    off_hz = tl.program_id(1)
-    qvk_offset = off_hz * stride_qh
-    Q_block_ptr = tl.make_block_ptr(
-        base=Q + qvk_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
-        strides=(stride_qm, stride_qk),
-        offsets=(start_m * BLOCK_M, 0),
-        block_shape=(BLOCK_M, BLOCK_DMODEL),
-        order=(1, 0)
-    )
-    DQ_block_ptr = tl.make_block_ptr(
-        base=DQ + qvk_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
-        strides=(stride_qm, stride_qk),
-        offsets=(start_m * BLOCK_M, 0),
-        block_shape=(BLOCK_M, BLOCK_DMODEL),
-        order=(1, 0)
-    )
-    K_block_ptr = tl.make_block_ptr(
-        base=K + qvk_offset,
-        shape=(BLOCK_DMODEL, N_CTX),
-        strides=(stride_kk, stride_kn),
-        offsets=(0, 0),
-        block_shape=(BLOCK_DMODEL, BLOCK_N),
-        order=(0, 1)
-    )
-    DK_block_ptr = tl.make_block_ptr(
-        base=DK + qvk_offset,
-        shape=(BLOCK_DMODEL, N_CTX),
-        strides=(stride_kk, stride_kn),
-        offsets=(0, 0),
-        block_shape=(BLOCK_DMODEL, BLOCK_N),
-        order=(0, 1)
-    )
-    V_block_ptr = tl.make_block_ptr(
-        base=V + qvk_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
-        strides=(stride_vk, stride_vn),
-        offsets=(0, 0),
-        block_shape=(BLOCK_N, BLOCK_DMODEL),
-        order=(1, 0)
-    )
-    DV_block_ptr = tl.make_block_ptr(
-        base=DV + qvk_offset,
         shape=(N_CTX, BLOCK_DMODEL),
         strides=(stride_vk, stride_vn),
         offsets=(0, 0),
@@ -288,9 +172,6 @@ def _bwd_kernel(
     tl.atomic_add(DFirst + off_h, dfirst)
 
 
-empty = torch.empty(128, device="cuda")
-
-
 class _attention(torch.autograd.Function):
 
     @staticmethod
@@ -365,10 +246,10 @@ def test_op(Z, H, N_CTX, D_HEAD, dtype=torch.float16):
     k = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0., std=0.5).requires_grad_()
     v = torch.empty((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda").normal_(mean=0., std=0.5).requires_grad_()
     dout = torch.randn_like(r)
-    # reference implementation
-    att_mask = torch.zeros(H, N_CTX, N_CTX, dtype=dtype, device="cuda")
     decay = torch.arange(0.9, 0.9 + H * 0.01, 0.01, device='cuda').requires_grad_()
     first = torch.arange(0.95, 0.95 + H * 0.001, 0.001, device='cuda').requires_grad_()
+    # reference implementation
+    att_mask = torch.zeros(H, N_CTX, N_CTX, dtype=dtype, device="cuda")
     rows, cols = torch.tril_indices(N_CTX, N_CTX, device='cuda')
     powers = rows - cols - 1
     for h in range(H):
@@ -412,7 +293,7 @@ configs = [triton.testing.Benchmark(
     ylabel='ms',
     plot_name=f'fused-attention-batch{BATCH}-head{N_HEADS}-d{D_HEAD}-{mode}',
     args={'H': N_HEADS, 'BATCH': BATCH, 'D_HEAD': D_HEAD, 'dtype': torch.float16, 'mode': mode}
-) for mode in ['bwd']]
+) for mode in ['fwd', 'bwd']]
 
 
 @triton.testing.perf_report(configs)
